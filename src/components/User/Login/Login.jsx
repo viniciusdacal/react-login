150ba3ca952aebbd94f5252c3ec3bed6da11a4ec
Luiz Claudio <lcluizclaudio96@gmail.com>
2021-01-22 14:49:22 -0300

    login e logout

--- src/components/User/Login/Login.jsx
+++ src/components/User/Login/Login.jsx
@@ -1,20 +1,88 @@
-import React from 'react';
-import UIButton from 'components/UI/Button/Button';
+import React, { useState, useContext} from "react";
+import {useHistory} from 'react-router-dom';
+import StoreContext from 'components/Store/Context';
+import UIButton from "components/UI/Button/Button";
 
-import './Login.css';
+import "./Login.css";
 
+function initalState() {
+  return {
+    user: '',
+    password: ''
+  };
+}
+
+function login({user, password}){
+  if(user === 'admin' && password === 'admin'){
+    return { token: '1234'}
+  }else{
+    return {error: 'Usuario ou senha invalidos!'}
+  }
+}
 const UserLogin = () => {
+
+  const [values, setValues] = useState(initalState);
+  const {setToken} = useContext(StoreContext);
+  const history = useHistory();
+  
+  function onChange(event) {
+    const { value, name } = event.target;
+    
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  }
+
+  function onSubmit(event){
+    event.preventDefault();
+
+    const {token} = login(values);
+
+    if(token){
+      setToken(token);
+      return history.push('/');
+    }
+
+    setValues(initalState);
+  }
+
   return (
-    <div className="user-login">
-      <h1 className="user-login__title">Acessar o Sistema</h1>
-      <form autoComplete="nope">
-        <div className="user-login__form-control">
-          <label htmlFor="email">E-mail</label>
-          <input id="email" type="text" name="email" autoComplete="off" />
+    <form autoComplete="nope" onSubmit={onSubmit}>
+      <div className="user-login">
+        <h1 className="user-login__title">Acessar o Sistema</h1>
+        <div className="form-group">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="user-login__form-control">
+                <label htmlFor="user">Usuario</label>
+                <input
+                  id="user"
+                  type="text"
+                  name="user"
+                  autoComplete="off"
+                  onChange={onChange}
+                  value={values.user}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="user-login__form-control">
-          <label htmlFor="password">Senha</label>
-          <input id="password" type="password" name="password" />
+        <div className="form-group">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="user-login__form-control">
+                <label htmlFor="password">Senha</label>
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  onChange={onChange}
+                  value={values.password}
+                />
+              </div>
+            </div>
+          </div>
         </div>
         <UIButton
           type="submit"
@@ -24,8 +92,8 @@ const UserLogin = () => {
         >
           Entrar
         </UIButton>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import './Home.css';
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import StoreContext from "components/Store/Context";
+import "./Home.css";
 
-const PagesHome = () => (
-  <div className="pages-home">
-    Parabéns, você conseguiu
-    <br />
-    <button type="button">Sair</button>
-  </div>
-);
+const PagesHome = () => {
+  const { setToken } = useContext(StoreContext);
+  const history = useHistory();
 
+  function sair(event) {
+    event.preventDefault();
+    setToken("");
+    console.log('sair');
+    return history.push('/login');
+  }
+
+  return (
+    <div className="pages-home">
+      Parabéns, você conseguiu!
+      <br />
+      <button type="button" onClick={sair}>
+        Sair
+      </button>
+    </div>
+  );
+};
 export default PagesHome;
